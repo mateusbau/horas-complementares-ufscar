ccc8bfd0dcdf10bb8784ed7a7cd74e5e06bf28aa
gclemos45 <gclemos61@gmail.com>
2026-09-16 22:45:15 -0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "SUA_PROJECT_URL_AQUI"
-const supabaseKey = "SUA_PUBLISHABLE_KEY_AQUI"
+const supabaseUrl = "https://twzrsevzgznanrquzith.supabase.co"
+const supabaseKey = "sb_publishable_siZPKBF1CsyyZr2tNt7IVQ__ElYMJpr"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
